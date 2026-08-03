06ea3219eb90bbeff862ce6453ad72451a876356
bcesano20 <54218077+bcesano20@users.noreply.github.com>
2026-08-03 12:58:56 +0200

    update version

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -50,7 +50,7 @@ export const Footer = () => {
           <p>
             &copy; {CURRENT_YEAR} {branding.appName}. Todos los derechos reservados.
             <br />
-            Version 1.1.0
+            Version 1.2.0
           </p>
           <p> Desarrollado por Bruno Cesano</p>
         </div>
